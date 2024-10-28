4490a9b2c1a4ccf58839a15eedbd8fcd34e8940e
ShahadathAlam <shahadathalam@ymail.com>
2024-10-28 14:28:41 +0600

    Creating a Dynamic Form with React Hook Form and React Query: Adding Cabins

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -6,6 +6,9 @@ import Button from "../../ui/Button";
 import FileInput from "../../ui/FileInput";
 import Textarea from "../../ui/Textarea";
 import { useForm } from "react-hook-form";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { createCabin } from "../../services/apiCabins";
 
 const FormRow = styled.div`
   display: grid;
@@ -44,9 +47,25 @@ const Error = styled.span`
 `;
 
 function CreateCabinForm() {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, reset } = useForm();
+  const queryClient = useQueryClient();
+  const { isLoading, mutate } = useMutation({
+    // mutationFn: newCabin => createCabin(newCabin),
+    mutationFn: createCabin,
+    onSuccess: (data) => {
+      console.log(data);
+      toast.success("New Cabin is successfully created");
+      queryClient.invalidateQueries({
+        queryKey: ["cabins"],
+      });
+      reset();
+    },
+
+    onError: (err) => toast.error(err.message),
+  });
+
   function onSubmit(data) {
-    console.log(data);
+    mutate(data);
   }
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
