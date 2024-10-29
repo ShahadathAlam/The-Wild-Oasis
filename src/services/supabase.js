dd2f134d86f24aa4c401ecccf584ed4f5f91605b
ShahadathAlam <shahadathalam@ymail.com>
2024-10-29 12:02:50 +0600

    Uploading Images to Supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://gmmxprbqcaxwnjnjeoeg.supabase.co";
+export const supabaseUrl = "https://gmmxprbqcaxwnjnjeoeg.supabase.co";
 
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdtbXhwcmJxY2F4d25qbmplb2VnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk3NjEyMTksImV4cCI6MjA0NTMzNzIxOX0.AgUAf5descLQIK5OhSLC7d1MukT0zDE2Ljm-LnW6yWM";
